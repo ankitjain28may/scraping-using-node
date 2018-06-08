577f63ccd21d2aab3f8d1b7b879456f7357156ad
ankitjain28may <ankitjain28may77@gmail.com>
2018-06-08 18:05:51 +0530

    Added async

--- nightmare.js
+++ nightmare.js
@@ -3,19 +3,24 @@ const nightmare = Nightmare({ show: true })
 const cheerio = require('cheerio');
 const fs = require('fs');
 
+// ************ gotoURL ****************
+
 gotoUrl = function(url) {
   return nightmare
     .goto(url)
-    .wait('#dp-container')
-    .evaluate(() => document.querySelector('#dp-container').innerHTML)
-    .end()
-    .then(function(body) {
-      console.log(body);
-    })
-    .catch(error => {
-      console.error('Search failed:', error)
-    })
+    .title()
+    // .wait('#dp-container')
+    // .evaluate(() => document.querySelector('#dp-container').innerHTML))
+    // .then(function(body) {
+    //   return body
+    // })
+    // .catch(error => {
+    //   console.error('Search failed:', error)
+    // }))
+    // )
 }
+
+// **** Need to write further pagination function and others functions*************
 
 
 // gotoUrl('https://www.amazon.in');
@@ -28,30 +33,58 @@ gotoUrl = function(url) {
 //   // nightmare
 //   return url
 // }
-readCategory = function (category = '') {
+//
+
+// *****************Working*************
+
+// readCategory = function (category = '') {
+//   fs.readFile('amazon_categories.json', (err, data) => {
+//     var Categories = JSON.parse(data);
+//     if (category != '') {
+//       Categories[category].reduce(function (total, ele) {
+//         return total.then(function(results) {
+//           return nightmare
+//             .goto(ele.link)
+//             // .wait('#dp-container')
+//             // .evaluate(() => document.querySelector('#dp-container').innerHTML)
+//             .title()
+//             // .end()
+//             .then(function(body) {
+//               results.push(body);
+//               return results;
+//             })
+//             .catch(error => {
+//               console.error('Search failed:', error)
+//             })
+//         })
+//       }, Promise.resolve([])).then(function(results){
+//         console.log(results);
+//         nightmare.end()
+//       });
+//     }
+//   });
+// }
+
+// *****************Not Working*************
+
+readCategory = function(category = '') {
   fs.readFile('amazon_categories.json', (err, data) => {
     var Categories = JSON.parse(data);
+    ca = [];
     if (category != '') {
-      Categories[category].reduce(function (accumulator, ele) {
-        console.log(accumulator);
-        console.log(ele.link);
-        return accumulator.then(function(results) {
-          return nightmare
+      const promises = Promise.all(Categories[category].forEach(async (ele) => {
+        try {
+          const data = await nightmare
             .goto(ele.link)
-            // .wait('#dp-container')
-            // .evaluate(() => document.querySelector('#dp-container').innerHTML)
             .title()
-            // .end()
-            .then(function(body) {
-              console.log("H");
-            })
-            .catch(error => {
-              console.error('Search failed:', error)
-            })
-        })
-      }, Promise.resolve([])).then(function(results){
-        console.log(results);
-      });
+          return data;
+        }
+        catch (e) {
+          console.log(e);
+        }
+      })).then((data) =>
+        console.log(data)
+      ).catch((err) => console.log(err));
     }
   });
 }
@@ -93,3 +126,38 @@ scrapeAllCategories = function (html, callback) {
 
 readCategory('Echo & Alexa');
 //
+
+// *****************Useless (Rough) *************
+
+
+// var euros = [29.76, 41.85, 46.5];
+
+// var sum = euros.reduce( function(total, amount){
+//   return total
+//     .then(function (amount) {
+//       return new Promise((resolve, reject) => {
+//         console.log("H");
+//         return resolve();
+//       });
+//     })
+//   }, Promise.resolve([])).then(function(results){
+//     console.log(results);
+//   });
+
+// function doubleAfter2Seconds(x) {
+//   return new Promise(resolve => {
+//     setTimeout(() => {
+//       resolve(x * 2);
+//     }, 2000);
+//   });
+// }
+// async function addAsync(x) {
+//   const a = await doubleAfter2Seconds(10);
+//   const b = await doubleAfter2Seconds(20);
+//   const c = await doubleAfter2Seconds(30);
+//   return x + a + b + c;
+// }
+
+// addAsync(10).then((sum) => {
+//   console.log(sum);
+// });
