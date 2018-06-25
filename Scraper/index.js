77c0a7b607d9b16fd1e8b5acb1dba528cbad81d6
ankitjain28may <ankitjain28may77@gmail.com>
2018-06-25 13:36:40 +0530

    Added innerpage

--- Scraper/index.js
+++ Scraper/index.js
@@ -14,6 +14,7 @@ app.get('/', (req, res) => {
 
 app.post('/get-static' ,async (req, res) => {
   var url = req.body.url;
+
   var results = {
     'status': '',
     'response': {},
@@ -24,19 +25,59 @@ app.post('/get-static' ,async (req, res) => {
       uri: url,
       method: 'GET',
       resolveWithFullResponse: true
-    }).then((response) => {
+    }).then(async (response) => {
       results.status = true;
       results.response = response;
+      [links, results.response.body] = getLink(response.body, req.body.options.context, req.body.options.link_selector);
+      const body = await Promise.all(links).then(values => {
+        return values;
+      });
+      results.response.body = getInnerPage(results.response.body, req.body.options.context, body, req.body.options.inner_page_selector);
     });
   }
   catch(err)  {
     results.status = false;
     results.error = err;
   }
-
   res.send(JSON.stringify(results));
 });
 
+getLink = function (html, context, link_selector) {
+  var selector = context + " " + link_selector;
+  var links = [];
+  const $ = cheerio.load(html);
+  try {
+    $(selector).each(function(i, elem) {
+      link = $(elem).attr('href');
+      links.push(
+        rp({
+          uri: link,
+          method: 'GET',
+          resolveWithFullResponse: true
+        })
+      );
+    });
+    return [links, $.html(context)];
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+getInnerPage = function (html, selector, pages, inner_page_selector) {
+  const $ = cheerio.load(html, {
+    normalizeWhitespace: true,
+  });
+  try {
+    $(selector).each(function(i, elem) {
+      const $c = cheerio.load(pages[i].body);
+        $(this).append('<affiliatesconnect>' + $c(inner_page_selector).html() + '</affiliatesconnect');
+    });
+    $('script').remove();
+    return $.html(selector);
+  } catch (e) {
+    console.log(e);
+  }
+}
 
 app.listen(port);
 console.log("Server started at port " + port);
